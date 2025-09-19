5466a25b7be99121e95298b16310fb3542ba9aa2
Harshkumar2284 <140580320+Harshkumar2284@users.noreply.github.com>
2025-09-19 15:45:17 +0530

    Teacher

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './Components/Navbar'
+import Login from './Components/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className=' text-2xl text-black font-bold underline'>AI PLANnerrrrrrrrr</h1>
+      <Navbar />
+      <Login />
     </>
   )
 }
---
+++ client/src/Components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+      <h1>Hello guys</h1>
+      <p>Hello bachcho</p>
+    </div>
+  )
+}
