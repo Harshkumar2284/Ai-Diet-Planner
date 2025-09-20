a41397d42e7fe28c1ec9452b74dc668fdb9af862
Harshkumar2284 <140580320+Harshkumar2284@users.noreply.github.com>
2025-09-20 10:22:29 +0530

    done

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -2,8 +2,17 @@ import React, { useState } from 'react';
 import GreyScreen from './GreyScreen';
 
 export default function Navbar() {
-  const [authenticate, setAuthenticate] = useState("hidden");
-
+  const [display, setDisplay] = useState("hidden");
+  const [authenticate,setAuthenticate] = useState("")
+  // Functions
+  const handleLoginDisplay = ()=>{
+    setDisplay("block")
+    setAuthenticate("login")
+  }
+  const handleSignUpDisplay = ()=>{
+    setDisplay("block")
+    setAuthenticate("register")
+  }
   return (
     <nav className=' w-full border-1 border-black py-4 bg-zinc-800 flex justify-around' style={{color:"#f07651"}}>
       <div>
@@ -15,10 +24,10 @@ export default function Navbar() {
         <li className=' font-semibold hover:text-orange-500 text-lg hover:cursor-pointer transition-colors duration-150 hover:underline ml-14'>For Professionals</li>
       </list>
       <div className='flex flex-col items-center'>
-        <button className='w-20 py-2 rounded-xl hover:cursor-pointer text-white font-semibold' style={{backgroundColor:"#f07651"}}>SignUp</button>
-        <h1 className='hover:underline hover:cursor-pointer'>Already a member? Sign In</h1>
+        <button className='w-20 py-2 rounded-xl hover:cursor-pointer text-white font-semibold' style={{backgroundColor:"#f07651"}} onClick={handleSignUpDisplay}>SignUp</button>
+        <h1 className='hover:underline hover:cursor-pointer' onClick={handleLoginDisplay}>Already a member? Sign In</h1>
       </div>
-      <GreyScreen display={authenticate} />
+      <GreyScreen display={display} authenticate = {authenticate}/>
     </nav>
   );
 }
